f516eef20d3ebaa8151abac777d3b2eddd3a43c5
Lawrence Brown <lgsb2@cam.ac.uk>
2021-02-07 16:16:03 +0000

    unify db schema

--- roomsurvey/schema.sql
+++ roomsurvey/schema.sql
@@ -58,3 +58,25 @@ CREATE TABLE form_payload (
 	time INTEGER NOT NULL,
 	json TEXT NOT NULL
 );
+
+DROP TABLE IF EXISTS room;
+
+CREATE TABLE room (
+	id INTEGER PRIMARY KEY,
+	shortname TEXT UNIQUE NOT NULL,
+	inhabitant TEXT DEFAULT NULL
+);
+
+DROP TABLE IF EXISTS review;
+
+CREATE TABLE review (
+	id INTEGER PRIMARY KEY,
+	author TEXT NOT NULL,
+	room INTEGER NOT NULL,
+	layout_rating INTEGER NOT NULL,
+	facilities_rating INTEGER NOT NULL,
+	noise_rating INTEGER NOT NULL,
+	overall_rating INTEGER NOT NULL,
+	title TEXT NOT NULL,
+	body TEXT NOT NULL
+);
